eaab1ac21a8e6ac30dcba113b8dacedcb3879c07
tejaswanama111 <tejaswanama2006@gmail.com>
2020-10-25 09:53:14 -0700

    database

--- sketch.js
+++ sketch.js
@@ -1,29 +1,50 @@
 var ball;
+var syncBall, database, position;
 
 function setup(){
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    syncBall = createSprite(250,250,10,10);
+    syncBall.shapeColor = "red";
+
+    database = firebase.database();
+
+    var ballPos = database.ref('ball/position');
+    ballPos.on("value", readPosition, showError);
 }
 
 function draw(){
     background("white");
+    if(position !== undefined){
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
+}
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+
+    database.ref('ball/position').set({
+        'x' : position.x + x,
+        'y' : position.y + y
+    });
+}
+
+function readPosition(data){
+    position = data.val();
+    syncBall.x = position.x;
+    syncBall.y = position.y;
+}
+
+function showError() {
+    console.log("error");
 }
